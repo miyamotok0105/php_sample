24292c7561f5f07aaaee2156aa7261f6cb5bace5
miyamoto <miyamotok0105@gmail.com>
2018-07-04 22:01:23 +0900

    Add manual

---
+++ manual_sample/001.php
@@ -0,0 +1,9 @@
+echo "特殊文字を HTML エンティティに変換する";
+
+echo htmlspecialchars('ようこそ', ENT_QUOTES, 'UTF-8');
+echo htmlspecialchars('こんにちは', ENT_QUOTES, 'UTF-8');
+echo htmlspecialchars('こんばんは', ENT_QUOTES, 'UTF-8');
+echo htmlspecialchars('さようなら', ENT_QUOTES, 'UTF-8');
+
+
+echo htmlspecialchars(date('G:i'));
